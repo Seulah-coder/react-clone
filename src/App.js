f92d7c6e4ce0a1ebd5262baaba866aae2d20be9a
Seulah Kim <seulahkim@chunjae.co.kr>
2022-12-14 15:42:54 +0900

    react movie clone coding

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import {BrowserRouter as Router, Routes, Route,} from "react-router-dom";
 import Home from "./routes/Home";
-import Movie from "./components/Movie";
+import Detail from "./routes/Detail";
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/movie" element={<Movie />}/>
+                <Route path="/movie/:id" element={<Detail />}/>
                 <Route path="/" element={<Home />}/>
             </Routes>
         </Router>
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,6 +1,23 @@
+import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 function Detail(){
-    return <h1>Detail</h1>
+    const {id} = useParams();
+    const [movie, setMovie] = useState([]);
+    const getMovie = async () => {
+        const json = await (await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)).json();
+        console.log(json.data.movie);
+        setMovie(json.data.movie);
+    }
+    useEffect(() => {
+        getMovie();
+
+    }, []);
+    return (<div> <h1>Detail</h1>
+        <h2>Title: {movie.title}</h2>
+    <p>Description: {movie.description_full}</p>
+        <p>Released year: {movie.year}</p>
+    </div>);
 }
 
 export default Detail;
