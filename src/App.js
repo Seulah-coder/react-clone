3f3f0800d1eabed930403e5649eea12707c72f75
Seulah Kim <seulahkim@chunjae.co.kr>
2022-12-14 09:44:48 +0900

    react movie clone coding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route,} from "react-router-dom";
+import Home from "./routes/Home";
+import Movie from "./components/Movie";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/movie" element={<Movie />}/>
+                <Route path="/" element={<Home />}/>
+            </Routes>
+        </Router>
+    )
+
 }
 
 export default App;
