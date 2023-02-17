9d39ecbed6359417f24583cc86337fef4dbeac4b
Seulah Kim <seulahkim@chunjae.co.kr>
2023-02-17 09:49:28 +0900

    상세페이지 안보여서 경로 다시 잡아줌

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,7 +5,7 @@ function Movie({id, coverImg, title, summary, genres}){
     console.log("333");
     return <div>
         <img src={coverImg} alt={title}/>
-        <h2><Link to ={`/movie/${id}`}>{title}</Link></h2>
+        <h2><Link to ={`${process.env.PUBLIC_URL}/movie/${id}`}>{title}</Link></h2>
         <p>{summary}</p>
         <ul>
             {genres.map((g) => (<li key={g}>{g}</li>))}
